80c4b8f2cd00587044b7773d9c763696cdc5f905
Sergio Xalambrí <hello@sergiodxa.com>
2019-05-29 12:57:03 -0500

    Add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Map, TileLayer, Marker } from "react-leaflet";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  const [latitude, setLatitude] = React.useState(0);
+  const [longitude, setLongitude] = React.useState(0);
+
+  React.useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(({ coords }) => {
+      setLatitude(coords.latitude);
+      setLongitude(coords.longitude);
+    });
+
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, [setLatitude, setLongitude]);
+
+  return (
+    <Map center={[latitude, longitude]} zoom={15}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | Made for Codeable'
+      />
+      <Marker position={[latitude, longitude]} />
+    </Map>
+  );
+}
+
+render(<App />, document.getElementById("root"));
